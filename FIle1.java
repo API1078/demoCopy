3e81ebdbd75d516abcabe73c59c779a2864daa51
API1078 <nitesh.sharma@apisero.com>
2021-04-24 13:14:19 +0530

    created dir and copied a file from one folder to another and now pushing it to remote

---
+++ FIle1.java
@@ -0,0 +1,14 @@
+class FIle1{
+	public static void main(String[] args){
+		System.out.println("First Line");
+		System.out.println("Second Line from master");
+		System.out.println("Second Line from branch Apisero");
+		System.out.println("Third Line from branch Apisero");
+		System.out.println("Third Line from branch master");
+		System.out.println("Checking dynamic varible in jenkins");
+		System.out.println("Checking second time dynamic varible in jenkins");
+		System.out.println("Checking third time dynamic varible in jenkins");
+		System.out.println("Checking fourth time dynamic varible in jenkins");
+		System.out.println("Checking final time dynamic varible in jenkins");
+	}
+}
